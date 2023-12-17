7e06642aa8c1ad85f1c64cf3e16e7306e63b2144
JonRahman <jonrahman90@gmail.com>
2023-12-17 19:52:51 +0000

    Added APIkey, added function drafts

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-var APIkey = ""
+var APIkey = "500a93d00042fdd6a229ad4c8e16ebbb"
 
 var enterCityBtn = $("#city-button")
 var searchBtn = $("#search-button")
-var pastSeachBtn = $("#past-search-button")
+// var pastSeachBtn = $("#past-search-button")
+
+var cityInput;
 
 function getWeather(data) {
 var baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&lon=${data.lon}&appid=${APIkey}`
@@ -10,3 +12,29 @@ var baseURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.lat}&
     .then(response => response.json())
 }
 
+function displaySearchHistory() {
+    // Get the history from local storage and parse it into an array of objects
+    
+}
+
+function getLocation() {
+    
+}
+
+function pastCity() {
+
+}
+
+function clearHistory() {
+
+}
+
+
+
+displaySearchHistory();
+
+searchBtn.on("click", handleCityFormSubmit);
+
+clearBtn.on("click", handleClearHistory);
+
+pastSearchedCitiesEl.on("click", getPastCity);
